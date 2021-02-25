b2cca397439b58e71e25f50eee06a92305cc4915
treguv <vladislavtregubov00@gmail.com>
2021-02-24 22:16:00 -0800

    Added friends list and indicidual user and thought pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
-
+//import other components
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 const client = new ApolloClient({
   uri: "/graphql", //react runs on 3000, server on 3001, need to specify abaolute path,
@@ -12,13 +18,25 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              {/* Render these components when the browser path matches */}
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              {/* ? means param is optional */}
+              <Route exact path="/profile/:username" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+              {/* Catch all non existing routes */}
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
